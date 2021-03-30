cb947b10c91698d48b89725f21d2396f77eac2bd
Nyilll <79820477+Nyilll@users.noreply.github.com>
2021-03-30 17:37:10 +1300

    30/3 update

---
+++ First 2/Assets/Rob/Scripts/EnemyTurret.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTurret : MonoBehaviour
+{ }
+   /* private GameObject player;
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((player.transform.position-this.transform.position) < 3.0f )
+        {
+            Debug.Log("Detected");
+        }
+    }
+}
+   */
---
+++ First 2/Assets/Rob/Scripts/WallScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallScript : MonoBehaviour
+    
+{
+    public GameObject BreakableWall;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        Debug.Log("Collision!");
+
+        if(other.gameObject.TryGetComponent(out PlayerBullet bullet))
+        {
+            Destroy(bullet.gameObject);
+            Destroy(BreakableWall.gameObject); 
+        }
+    }
+}
